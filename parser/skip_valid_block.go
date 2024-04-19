b9cadac3c674eb91a126c58fd16042041cb97235
Tufan Barış Yıldırım <tufanbarisyildirim@gmail.com>
2024-04-19 15:17:47 +0300

    make linter happy

--- parser/skip_valid_block.go
+++ parser/skip_valid_block.go
@@ -6,6 +6,8 @@ var skipValidBlocks = `types
 map
 `
 
+// SkipValidBlocks defines a list of valid blocks to be skipped during initialization.
+// This string is split by newline characters, with each trimmed block name added to the SkipValidBlocks mapping.
 var SkipValidBlocks map[string]struct{} = map[string]struct{}{}
 
 func init() {
