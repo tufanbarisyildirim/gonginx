11f4b231bbbe52d3eff7b2daa130edc6915e204c
zhangronghan001 <zhangronghan001@ke.com>
2024-07-05 14:33:18 +0800

    fix unexpected token BlockEnd (})

--- parser/parser.go
+++ parser/parser.go
@@ -258,6 +258,9 @@ func (p *Parser) parseStatement(isSkipValidDirective bool) (config.IDirective, e
 	//parse parameters until the end.
 	for p.nextToken(); p.currentToken.IsParameterEligible(); p.nextToken() {
 		d.Parameters = append(d.Parameters, p.currentToken.Literal)
+		if p.followingToken.Is(token.BlockEnd) {
+			return d, nil
+		}
 	}
 
 	//if we find a semicolon it is a directive, we will check directive converters
